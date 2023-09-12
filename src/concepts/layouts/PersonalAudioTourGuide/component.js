d7b01700bceec7225667890b3e92cc95aeb7b94a
soularis109 <soularis109@gmail.com>
2023-09-12 16:16:13 +0300

    margin

--- src/concepts/layouts/PersonalAudioTourGuide/component.js
+++ src/concepts/layouts/PersonalAudioTourGuide/component.js
@@ -8,23 +8,25 @@ const PersonalAudioTourGuide = () => {
     return (
         <div className={styles.body}>
             <div className={'container'}>
-            <div className={styles.body__title}>
-                <FormattedMessage id={'personal.text'} />
-            </div>
-            <div className={styles.body__sub__title}>
-                <FormattedMessage id={'discover.text'} />
-            </div>
-            <div className={styles.body__AppBlock}>
-                {AppDownload.map((item,index) => {
-                    return (
-                        <div key={index} className={styles.body__AppBlock__item}>
-                            <Link href={item.link} target={item.target}>
-                                <img src={item.icon} alt={item.name} className={styles.body__AppBlock__img}/>
-                            </Link>
-                        </div>
-                    )
-                })}
-            </div>
+                <div className={styles.body__block}>
+                    <div className={styles.body__title}>
+                        <FormattedMessage id={'personal.text'} />
+                    </div>
+                    <div className={styles.body__sub__title}>
+                        <FormattedMessage id={'discover.text'} />
+                    </div>
+                    <div className={styles.body__AppBlock}>
+                        {AppDownload.map((item,index) => {
+                            return (
+                                <div key={index} className={styles.body__AppBlock__item}>
+                                    <Link href={item.link} target={item.target}>
+                                        <img src={item.icon} alt={item.name} className={styles.body__AppBlock__img}/>
+                                    </Link>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     );
